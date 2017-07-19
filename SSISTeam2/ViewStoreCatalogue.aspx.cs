27c3e4886e492964615bfb9a6a6133ddb067716d
xinyi <470352443@qq.com>
2017-07-19 14:26:36 +0800

    Connected EF (xinyi)

--- SSISTeam2/ViewStoreCatalogue.aspx.cs
+++ SSISTeam2/ViewStoreCatalogue.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace SSISTeam2
 {
-    public partial class MonthlyCheck : System.Web.UI.Page
+    public partial class ViewStoreCatalogue : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SSISEntities s = new SSISEntities();
+
         }
     }
 }
