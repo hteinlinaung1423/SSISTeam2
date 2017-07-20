b33d4e0caf61ea4c77dc8f0c24c6a20fc0b7196f
YangHuitian <Yang@YANG-N552VW>
2017-07-20 13:46:05 +0800

    Worked on Employee request history

--- SSISTeam2/Views/Employee/EmpRequestHistory.aspx.cs
+++ SSISTeam2/Views/Employee/EmpRequestHistory.aspx.cs
@@ -5,13 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace SSISTeam2
+namespace SSISTeam2.Views.Employee
 {
-    public partial class MonthlyCheck : System.Web.UI.Page
+    public partial class EmpRequestHistory : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+
         }
     }
 }
