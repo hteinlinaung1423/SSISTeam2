19e63aaa1a76df440f4066773547ad7399338dc3
HengTiong <heng_tiong@hotmail.com>
2017-07-20 09:40:35 +0800

    testing roles

--- SSISTeam2/MonthlyCheck.aspx.cs
+++ SSISTeam2/MonthlyCheck.aspx.cs
@@ -15,10 +15,10 @@ namespace SSISTeam2
         protected void Page_Load(object sender, EventArgs e)
         {
             SSISEntities context = new SSISEntities();
-            UserModel um = new UserModel("hengtiong", context);
+            UserModel um = new UserModel(HttpContext.Current.User.Identity.Name, context);
             today = DateTime.Today;
             DateTB.Text = today.Date.ToString("dd/MM/yyyy");
-            Label1.Text = um.ContactNumber + um.Email + um.Username;
+            Label1.Text = um.ContactNumber + um.Email + um.Username + um.Role;
             TableRow tr = new TableRow();
             Table1.Rows.Add(tr);
         }
