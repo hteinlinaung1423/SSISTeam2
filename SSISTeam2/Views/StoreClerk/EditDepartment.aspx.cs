fe14459f90be05ac3518732bd7745b80c7633ecf
e0167350 <e0167350@ISSBOR31405>
2017-07-20 14:32:05 +0800

    Waddy

--- SSISTeam2/Views/StoreClerk/EditDepartment.aspx.cs
+++ SSISTeam2/Views/StoreClerk/EditDepartment.aspx.cs
@@ -7,13 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace SSISTeam2.Views.StoreClerk
 {
-    public partial class ViewDepartmentList : System.Web.UI.Page
+    public partial class EditDepartment : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SSISEntities s = new SSISEntities();
-            GridView1.DataSource = s.Departments.ToList<Department>();
-            GridView1.DataBind();
+
         }
     }
 }
