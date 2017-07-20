3e6fdc3edf9d3f6412cc6369abca2af9b3123ed6
leonardchow <leonardchowpk@gmail.com>
2017-07-21 02:41:50 +0800

    Finished Request Lifecycle movement service

---
+++ SSISTeam2/Classes/EFFServices/IDisbursementService.cs
@@ -0,0 +1,18 @@
+﻿using SSISTeam2.Classes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSISTeam2.Classes.EFFServices
+{
+    public interface IDisbursementService
+    {
+        DisbursementModelCollection getAllDisbursementsForCollectionPoint(int collectionPointId);
+        DisbursementModelCollection getAllDisbursementsFromDepartment(string deptCode);
+        DisbursementModelCollection getAllDisbursements();
+        DisbursementModel findLatestDisbursementByRequestId(int requestId);
+        int markRequestAsDisbursed(RequestModel toAllocate, string currentUser);
+    }
+}
