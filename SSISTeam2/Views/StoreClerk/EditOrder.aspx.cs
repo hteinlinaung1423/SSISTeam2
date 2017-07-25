4d4fea3395928145a5f9abbebdf4bb4e5ac1c97d
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-07-25 09:49:51 +0800

    DeliveryOrder Done

--- SSISTeam2/Views/StoreClerk/EditOrder.aspx.cs
+++ SSISTeam2/Views/StoreClerk/EditOrder.aspx.cs
@@ -10,15 +10,33 @@ namespace SSISTeam2.Views.StoreClerk
     public partial class EditOrder : System.Web.UI.Page
     {
         SSISEntities ctx = new SSISEntities();
-        int orderId;
+          int orderId;
         protected void Page_Load(object sender, EventArgs e)
         {
+            orderId = Convert.ToInt32(Session["order"]);
+
             if (!IsPostBack)
             {
-                orderId =Convert.ToInt32(Request.QueryString["order"]) ;
-                GridView1.DataSource = ctx.Purchase_Order_Details.Where(x => x.order_id == orderId && x.deleted != "Y").Select(x=> new {x.Tender_List_Details.Stock_Inventory.item_description, x.quantity, x.status, x.order_details_id }).ToList();
+                               
+                var list = ctx.Purchase_Order_Details.Where(x => x.order_id == orderId && x.deleted != "Y").Select(x => new { x.Tender_List_Details.Stock_Inventory.item_description, x.quantity, x.status, x.order_details_id }).ToList();
+                GridView1.DataSource = list;
                 GridView1.DataBind();
+                if (list.Count != 0)
+                {
+                    finish.Visible = true;
+                }
+                else
+                {
+                    lblResult.Visible = true;
+                    lblResult.Text = "Oh Snap! Nothing to show!";
+                    finish.Visible = false;
+                }
             }
+           
+
+
+
+
         }
 
         protected void delete_OrderItem(object sender, EventArgs e)
@@ -26,20 +44,25 @@ namespace SSISTeam2.Views.StoreClerk
             Button btn = (Button)sender;
             GridViewRow gvr = (GridViewRow)btn.NamingContainer;
 
-            int orderId = Convert.ToInt32(((Label)gvr.FindControl("Label_OrderDetailId")).Text);
+            int orderDetailId = Convert.ToInt32(((Label)gvr.FindControl("Label_OrderDetailId")).Text);
 
-            Purchase_Order_Details order = ctx.Purchase_Order_Details.Where(x => x.order_details_id == orderId).First();
+            Purchase_Order_Details order = ctx.Purchase_Order_Details.Where(x => x.order_details_id == orderDetailId).First();
+
+            
 
             order.deleted = "Y";
 
             ctx.SaveChanges();
 
-            Response.Redirect("~/Views/StoreClerk/ViewPendingOrder.aspx");
+            Response.Redirect("~/Views/StoreClerk/EditOrder.aspx");
         }
 
         protected void ReceiveOrder(object sender, EventArgs e)
         {
-            Response.Redirect("~/Views/StoreClerk/ReceiveOrder.aspx?orderId="+orderId);
+            Purchase_Order p =ctx.Purchase_Order.Where(x => x.order_id == orderId).First();
+            Session["suppliername"] = p.Supplier.name;
+
+            Response.Redirect("~/Views/StoreClerk/ReceiveOrder.aspx");
         }
     }
 }
--- SSISTeam2/Views/StoreClerk/ReceiveOrder.aspx.cs
+++ SSISTeam2/Views/StoreClerk/ReceiveOrder.aspx.cs
@@ -9,9 +9,76 @@ namespace SSISTeam2.Views.StoreClerk
 {
     public partial class ReceiveOrder : System.Web.UI.Page
     {
+        SSISEntities ctx = new SSISEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+            if (!IsPostBack)
+            {
+
+                int orderID = Convert.ToInt32(Session["order"]);
+
+                
+
+                GridView1.DataSource = ctx.Purchase_Order_Details.Where(x => x.order_id == orderID && x.status == "Pending").Select(x => new { x.order_details_id, x.Tender_List_Details.Stock_Inventory.item_description, x.quantity }).ToList();
+                GridView1.DataBind();
+            }
+
+
         }
+
+        protected void Confirm(object sender, EventArgs e)
+        {
+            int orderID = Convert.ToInt32(Session["order"]);
+
+            Delivery_Orders d = new Delivery_Orders();
+
+            d.clerk_user = User.Identity.Name;
+            d.deleted = "N";
+            d.order_id = orderID;
+            d.receive_date = DateTime.Parse(deliverydate.Text);
+
+            ctx.Delivery_Orders.Add(d);
+            ctx.SaveChanges();
+
+            //Get DeliveryOrderID
+            Delivery_Orders order = ctx.Delivery_Orders.Where(x => x.clerk_user == User.Identity.Name).OrderBy(x => x.delivery_id).ToList().Last();
+
+            foreach (GridViewRow rows in GridView1.Rows)
+            {
+                TextBox qty = (TextBox)rows.FindControl("quantity");
+                TextBox r = (TextBox)rows.FindControl("remark");
+
+                Label orderdetailid=(Label)rows.FindControl("Label_OrderDetailId");
+
+
+                int quantity =Convert.ToInt32( qty.Text);
+                string remarks = r.Text;
+                int orderDetail =Convert.ToInt32( orderdetailid.Text);
+
+               
+
+                Purchase_Order_Details pod = ctx.Purchase_Order_Details.Where(x => x.order_details_id == orderDetail).First();
+
+                Delivery_Details dd = new Delivery_Details();
+                dd.delivery_id = order.delivery_id;
+                dd.deleted = "N";
+                dd.tender_id = pod.tender_id;
+                dd.quantity = quantity;
+                dd.remarks = remarks;
+
+                ctx.Delivery_Details.Add(dd);
+                ctx.SaveChanges();
+
+            }
+
+
+            Response.Redirect("~/default.aspx");
+        }
+
+
     }
+
+    
 }
