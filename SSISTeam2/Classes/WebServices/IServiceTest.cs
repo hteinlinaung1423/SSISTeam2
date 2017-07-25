cad2c093d96e1cd6f8f97fa50803968a775a6089
leonardchow <leonardchowpk@gmail.com>
2017-07-26 01:11:49 +0800

    Implemented Web Service

---
+++ SSISTeam2/Classes/WebServices/IServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SSISTeam2.Classes.WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceTest
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/TestEmit", ResponseFormat = WebMessageFormat.Json)]
+        string TestEmit();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/TestEmit2", ResponseFormat = WebMessageFormat.Json)]
+        List<string> TestEmit2();
+    }
+}
