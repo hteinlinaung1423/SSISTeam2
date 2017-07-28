9b22565c5103e7242ab91e9bf22303df16d09bb4
KSYeo <e0167359@u.nus.edu>
2017-07-28 14:46:51 +0800

    Modifications to new navbar button codes

--- SSISTeam2/MasterPage.Master.designer.cs
+++ SSISTeam2/MasterPage.Master.designer.cs
@@ -220,6 +220,15 @@ namespace SSISTeam2 {
         protected global::System.Web.UI.WebControls.Button ViewCatalogue4;
         
         /// <summary>
+        /// ChangeCPRep2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ChangeCPRep2;
+        
+        /// <summary>
         /// logout control.
         /// </summary>
         /// <remarks>
