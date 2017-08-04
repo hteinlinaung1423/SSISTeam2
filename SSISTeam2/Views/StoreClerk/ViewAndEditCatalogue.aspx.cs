89e50d2f01741d778896c5e5e88eca53d681f8dd
May Oo Lwin <ms.mayoolwin@gmail.com>
2017-08-04 10:58:53 +0800

    Changes for ViewandEditCatalogue of next button click

--- SSISTeam2/Views/StoreClerk/ViewAndEditCatalogue.aspx.cs
+++ SSISTeam2/Views/StoreClerk/ViewAndEditCatalogue.aspx.cs
@@ -21,30 +21,66 @@ namespace SSISTeam2.Views.StoreClerk
         private void BindGrid()
 
         {
-
-            List<Stock_Inventory> SIList = entities.Stock_Inventory.Where(x => x.deleted.Equals("N")).ToList();
-            list = new List<ViewandEditCatalogueForShow>();
-            foreach (Stock_Inventory si in SIList)
+            string searchWord = TextBox1.Text;
+            if (!string.IsNullOrEmpty(searchWord))
             {
-                ViewandEditCatalogueForShow vec = new ViewandEditCatalogueForShow();
-                vec.itemNumber = si.item_code;
-                vec.Description = si.item_description;
+                list = new List<ViewandEditCatalogueForShow>();
+                var catList = entities.Categories.Where(x => x.deleted == "N" && x.cat_name.Contains(searchWord)).Select(x => x.cat_id).ToList();
+                var result2 = (from t1 in entities.Categories
+                               join t2 in entities.Stock_Inventory
+                               on t1.cat_id equals t2.cat_id
+                               where t1.deleted.Equals("N")
+                               && t2.deleted.Equals("N")
+                               && (catList.Contains(t2.cat_id))
+                               || t2.item_description.Contains(searchWord)
+                               || t2.item_code.Contains(searchWord)
+                               orderby t1.cat_name
+                               select new { t2.item_code, t1.cat_name, t2.item_description, t2.current_qty, t2.reorder_level, t2.reorder_qty, t2.unit_of_measure }).ToList();
 
-                vec.categoryName = (from t1 in entities.Categories
-                                    where t1.deleted.Equals("N")
-                                     && (t1.cat_id == si.cat_id)
-                                    select new { t1.cat_name }).ToList().First().cat_name;
-                vec.RL = si.reorder_level;
-                vec.RQ = si.reorder_qty;
-                vec.UoM = si.unit_of_measure;
-                vec.CQ = si.current_qty;
-                vec.CategoryId = si.cat_id;
+                for (int i = 0; i < result2.Count(); i++)
+                {
+                    ViewandEditCatalogueForShow vec = new ViewandEditCatalogueForShow();
+                    vec.itemNumber = result2[i].item_code;
+                    vec.categoryName = result2[i].cat_name;
+                    vec.Description = result2[i].item_description;
+                    vec.CQ = result2[i].current_qty;
+                    vec.RQ = result2[i].reorder_level;
+                    vec.RL = result2[i].reorder_level;
+                    vec.UoM = result2[i].unit_of_measure;
+                    list.Add(vec);
+                }
 
-                list.Add(vec);
+                GridView1.DataSource = list;
+
             }
-            GridView1.Columns[1].Visible = false;
-            GridView1.DataSource = list;
+            else
+            {
+                List<Stock_Inventory> SIList = entities.Stock_Inventory.Where(x => x.deleted.Equals("N")).ToList();
+                list = new List<ViewandEditCatalogueForShow>();
+                foreach (Stock_Inventory si in SIList)
+                {
+                    ViewandEditCatalogueForShow vec = new ViewandEditCatalogueForShow();
+                    vec.itemNumber = si.item_code;
+                    vec.Description = si.item_description;
+
+                    vec.categoryName = (from t1 in entities.Categories
+                                        where t1.deleted.Equals("N")
+                                         && (t1.cat_id == si.cat_id)
+                                        select new { t1.cat_name }).ToList().First().cat_name;
+                    vec.RL = si.reorder_level;
+                    vec.RQ = si.reorder_qty;
+                    vec.UoM = si.unit_of_measure;
+                    vec.CQ = si.current_qty;
+                    vec.CategoryId = si.cat_id;
+
+                    list.Add(vec);
+                }
+                GridView1.Columns[1].Visible = false;
+                GridView1.DataSource = list;
+
+            }
             GridView1.DataBind();
+
         }
         protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
         {
@@ -67,10 +103,10 @@ namespace SSISTeam2.Views.StoreClerk
             this.BindGrid();
             List<Category> categoryList = entities.Categories.ToList();
             List<string> categoryNameList = new List<string>();
-            foreach(Category c in categoryList)
+            foreach (Category c in categoryList)
             {
                 categoryNameList.Add(c.cat_name);
-            } 
+            }
             (GridView1.Rows[e.NewEditIndex].FindControl("DropDownList1") as DropDownList).Visible = true;
             (GridView1.Rows[e.NewEditIndex].FindControl("DropDownList1") as DropDownList).DataSource = categoryNameList;
             (GridView1.Rows[e.NewEditIndex].FindControl("DropDownList1") as DropDownList).DataBind();
@@ -88,7 +124,7 @@ namespace SSISTeam2.Views.StoreClerk
             string itemCode = (row.FindControl("Label1") as Label).Text;
             //int categoryID = Convert.ToInt32((row.FindControl("Label9") as Label).Text);
             var result1 = entities.Stock_Inventory.SingleOrDefault(x => x.item_code == itemCode);
-           // result1.current_qty = curQty;
+            // result1.current_qty = curQty;
             result1.reorder_level = reorderLevel;
             result1.reorder_qty = reorderQuantity;
             result1.unit_of_measure = unitOfMeasure;
@@ -114,7 +150,7 @@ namespace SSISTeam2.Views.StoreClerk
             GridViewRow row = GridView1.Rows[e.RowIndex];
 
             string itemCode = (row.FindControl("Label1") as Label).Text;
-           
+
             var result1 = entities.Stock_Inventory.SingleOrDefault(x => x.item_code == itemCode);
             result1.deleted = "Y";
             entities.SaveChanges();
@@ -137,36 +173,51 @@ namespace SSISTeam2.Views.StoreClerk
         }
         protected void Search_Click(object sender, EventArgs e)
         {
-            string searchWord = TextBox1.Text;
-            
-            list = new List<ViewandEditCatalogueForShow>();
-            var catList = entities.Categories.Where(x => x.deleted == "N" && x.cat_name.Contains(searchWord)).Select(x => x.cat_id).ToList();
-            var result2 = (from t1 in entities.Categories
-                           join t2 in entities.Stock_Inventory
-                           on t1.cat_id equals t2.cat_id
-                           where t1.deleted.Equals("N")
-                           && t2.deleted.Equals("N")
-                           && (catList.Contains(t2.cat_id))
-                           || t2.item_description.Contains(searchWord)
-                           || t2.item_code.Contains(searchWord)
-                           orderby t1.cat_name
-                           select new { t2.item_code, t1.cat_name, t2.item_description, t2.current_qty, t2.reorder_level, t2.reorder_qty, t2.unit_of_measure }).ToList();
 
-            for (int i = 0; i < result2.Count(); i++)
+            this.BindGrid();
+
+
+        }
+        protected void DropDownList_JumpToPage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow topPagerRow = GridView1.TopPagerRow;
+            GridViewRow bottomPagerRow = GridView1.BottomPagerRow;
+
+            DropDownList topJumpToPage = (DropDownList)topPagerRow.FindControl("DropDownList_JumpToPage");
+            DropDownList bottomJumpToPage = (DropDownList)bottomPagerRow.FindControl("DropDownList_JumpToPage");
+
+            if ((DropDownList)sender == bottomJumpToPage)
             {
-                ViewandEditCatalogueForShow vec = new ViewandEditCatalogueForShow();
-                vec.itemNumber = result2[i].item_code;
-                vec.categoryName = result2[i].cat_name;
-                vec.Description = result2[i].item_description;
-                vec.CQ = result2[i].current_qty;
-                vec.RQ = result2[i].reorder_level;
-                vec.RL = result2[i].reorder_level;
-                vec.UoM = result2[i].unit_of_measure;
-                list.Add(vec);
+                GridView1.PageIndex = bottomJumpToPage.SelectedIndex;
+            }
+            else
+            {
+                GridView1.PageIndex = topJumpToPage.SelectedIndex;
+
+            }
+            this.BindGrid();
+        }
+
+        protected void GridView_EditBooks_DataBound(object sender, EventArgs e)
+        {
+            GridViewRow topPagerRow = GridView1.TopPagerRow;
+            GridViewRow bottomPagerRow = GridView1.BottomPagerRow;
+
+            DropDownList topJumpToPage = (DropDownList)topPagerRow.FindControl("DropDownList_JumpToPage");
+            DropDownList bottomJumpToPage = (DropDownList)bottomPagerRow.FindControl("DropDownList_JumpToPage");
+
+            if (topJumpToPage != null)
+            {
+                for (int i = 0; i < GridView1.PageCount; i++)
+                {
+                    ListItem item = new ListItem("Page " + (i + 1));
+                    topJumpToPage.Items.Add(item);
+                    bottomJumpToPage.Items.Add(item);
+                }
             }
 
-            GridView1.DataSource = list;
-            GridView1.DataBind();
+            topJumpToPage.SelectedIndex = GridView1.PageIndex;
+            bottomJumpToPage.SelectedIndex = GridView1.PageIndex;
         }
     }
 
@@ -186,4 +237,3 @@ namespace SSISTeam2.Views.StoreClerk
     }
 
 }
-    
